2788e2a7713e8d12dca166843a66e0f6190678a4
Ariel <arielwernick@gmail.com>
2025-01-02 14:10:17 -0500

    Remove unused Book-related classes and HomeController to streamline the application. This includes the deletion of BookService, BookServiceImpl, Book, BookNotFoundException, and the HomeController, which were not utilized in the current architecture.

--- src/main/java/com/wernick/bookshelf/core/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.wernick.bookshelf.core.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(Long id) {
-        super("Could not find book with id: " + id);
-    }
-} 
--- src/main/java/com/wernick/bookshelf/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wernick.bookshelf.repository;
-
-import com.wernick.bookshelf.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-} 
